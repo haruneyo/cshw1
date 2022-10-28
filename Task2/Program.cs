56965d9879db0dc509189599b547b218dc0e95a2
leonid ivanov <leonid.ivanov.w@gmail.com>
2022-10-28 16:18:00 +0500

    Task 2 done

---
+++ Task2/Program.cs
@@ -0,0 +1,9 @@
+﻿int numA = new Random().Next(-100, 100);
+int numB = new Random().Next(-100, 100);
+
+Console.WriteLine(numA);
+Console.WriteLine(numB);
+
+if (numA > numB) Console.WriteLine(numA);
+else
+    Console.WriteLine(numB);
