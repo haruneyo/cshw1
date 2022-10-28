9be7f5254502be00c462192f7d008c3cb057f44e
leonid ivanov <leonid.ivanov.w@gmail.com>
2022-10-28 16:31:42 +0500

    Task 4 done

---
+++ Task4/Program.cs
@@ -0,0 +1,17 @@
+﻿int numA = new Random().Next(-100, 100);
+int numB = new Random().Next(-100, 100);
+int numC = new Random().Next(-100, 100);
+
+Console.WriteLine(numA);
+Console.WriteLine(numB);
+Console.WriteLine(numC);
+
+if (numA > numB) {
+    if (numA > numC) Console.WriteLine(numA);
+    else Console.WriteLine(numC);  
+}
+
+else {
+    if (numB > numC) Console.WriteLine(numB);
+    else Console.WriteLine(numC);
+}
